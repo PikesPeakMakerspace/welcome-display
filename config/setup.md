280d84b1b273645a2026bf7cd2dbc4bd928c157b
Pavan D <dayalpavan@gmail.com>
2022-07-19 20:29:10 -0600

    setup pi instructions

---
+++ config/setup.md
@@ -0,0 +1,4 @@
+# Setup
+* install raspbian on pi
+* clone `https://github.com/PikesPEakMakerspace/welcome-display.git` to desktop
+* copy the `autostart` script to `/etc/xdg/lxsession/LXDE-pi/autostart`
--- main.sh
+++ main.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 url="http://thesecatsdonotexist.com"
-chromium-browser --kiosk --start-fullscreen "$url"
+
+init() {
+    cd /home/pi/Desktop/welcome-display
+    git pull
+    #TODO start web app `npm run &` or `node index.js &`
+}
+
+main() {
+    chromium-browser --kiosk --start-fullscreen "$url"
+}
+
+init
+main
